b01978a3517a14b954977f41c282e6275f0b3a73
seron-ux <abyseronkirui@gmail.com>
2021-02-20 20:52:34 +0300

    created app cofigurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from .config import config_options
 
 # Initializing application
-# app = Flask(__name__)
+def create_app(config_name):
+app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(config_options[config_name])
+
---
+++ run.py
@@ -0,0 +1,4 @@
+from app import creat_app
+
+if __name__ == '__main__':
+    app.run(debug = True)
