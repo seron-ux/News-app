77f6be4b9c0dad32f6bcaa6e9a52e10b8c9f23b1
seron-ux <abyseronkirui@gmail.com>
2021-02-21 12:49:21 +0300

    created a bootstrap navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-# from flask_bootstrap import Bootstrap
-# from config import config_options
+from flask_bootstrap import Bootstrap
+from config import config_options
 
 bootstrap = Bootstrap()
 
