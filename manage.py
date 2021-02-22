d22b256b26fb9fa2bb77658952139b9ddebb8f8c
seron-ux <abyseronkirui@gmail.com>
2021-02-22 20:12:08 +0300

    added a readme.md file

--- manage.py
+++ manage.py
@@ -14,6 +14,8 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+# if __name__ == '__main__':
+#     manager.run()
 
 if __name__ == '__main__':
     manager.run()
